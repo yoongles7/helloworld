99361b765f1ae7307143d5265134bf53e327c25d
pritika <pritikas820@gmail.com>
2022-06-20 15:35:26 +0545

    statement filled

--- howold.c
+++ howold.c
@@ -2,13 +2,12 @@
 
 int main(){ 
 int year1, year2, age;
-
 printf("Enter your birth year-");
 scanf("%d",&year1);
-
 printf("Enter current year");
 scanf("%d",&year2);
 
+age = (year2 - year1);
 printf("You are %d years old.",age);
 return 0;
 }
