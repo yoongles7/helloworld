0384bc63d10a8fe6d77f34b1994b01a952b71f49
pritika <pritikas820@gmail.com>
2022-06-20 15:33:58 +0545

    calcualte age

---
+++ howold.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){ 
+int year1, year2, age;
+
+printf("Enter your birth year-");
+scanf("%d",&year1);
+
+printf("Enter current year");
+scanf("%d",&year2);
+
+printf("You are %d years old.",age);
+return 0;
+}
